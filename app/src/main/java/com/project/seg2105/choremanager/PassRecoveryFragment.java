1e989d71c1797dda2258c3437427e839a59cab29
Nyfy <PrimitiveStack000>
2017-12-06 12:25:43 -0500

    Schedule View activity working!!!

--- app/src/main/java/com/project/seg2105/choremanager/PassRecoveryFragment.java
+++ app/src/main/java/com/project/seg2105/choremanager/PassRecoveryFragment.java
@@ -3,6 +3,7 @@ package com.project.seg2105.choremanager;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -21,6 +22,11 @@ public class PassRecoveryFragment extends DialogFragment {
     EditText recoveryInput;
     String user;
     String recovery;
+
+    public void onAttach(Context context){
+        super.onAttach(context);
+        listener = (RecoveryDialogListener) context;
+    }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState){
